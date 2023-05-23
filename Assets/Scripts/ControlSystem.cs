ae4ce53c1261c75dd80775d3ce0fd492833d7614
asd1422054 <131770656+asd1422054@users.noreply.github.com>
2023-05-23 21:32:33 +0800

    update完成控制器

---
+++ Assets/Scripts/ControlSystem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ControlSystem : MonoBehaviour
+{
+    private void Awake()
+    {
+        print("測試一");
+        print(1 + 2);
+        print($"錢{1 + 2}");
+        print("1+2");
+    }
+
+    private void Start()
+    {
+        print("<color=red>開始事件</color>");
+    }
+    private void Update()
+    {
+        print("<color=blue>更新事件</color>");
+    }
+}
