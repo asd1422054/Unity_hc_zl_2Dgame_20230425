864894df5b41a9c8f152d8f2f18233b23b5f2f5a
asd1422054 <131770656+asd1422054@users.noreply.github.com>
2023-05-25 20:02:57 +0800

    update完成角色移動

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -2,20 +2,35 @@
 
 public class ControlSystem : MonoBehaviour
 {
+    [Header("移動速度")]
+    [Range(0,20)]
+    public float moveSpeed = 3.5f;
+    [Header("鋼體")]
+    public Rigidbody2D rig;
     private void Awake()
     {
-        print("測試一");
-        print(1 + 2);
-        print($"錢{1 + 2}");
-        print("1+2");
+        
     }
 
     private void Start()
     {
-        print("<color=red>開始事件</color>");
+       
     }
+
     private void Update()
     {
-        print("<color=blue>更新事件</color>");
+      //呼叫移動方法
+        Move();
     }
+
+    private void Move()
+	{
+        float h = Input.GetAxis("Horizontal");
+        print("水平值:" + h);
+        float v = Input.GetAxis("Vertical");
+        print("垂直值:" + v);
+
+        rig.velocity = new Vector2(h, v) * moveSpeed;
+	}
+
 }
