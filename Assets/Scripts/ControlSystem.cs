1609e8c95796599a27579065dc414debe8d081eb
asd1422054 <131770656+asd1422054@users.noreply.github.com>
2023-05-25 21:18:37 +0800

    update完成角色移動動畫

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -5,8 +5,16 @@ public class ControlSystem : MonoBehaviour
     [Header("移動速度")]
     [Range(0,20)]
     public float moveSpeed = 3.5f;
+
     [Header("鋼體")]
     public Rigidbody2D rig;
+
+    [Header("動畫控制器")]
+    public Animator ani;
+
+    [Header("跑步參數")]
+    public string parRun = "開關走路";
+
     private void Awake()
     {
         
@@ -27,10 +35,14 @@ public class ControlSystem : MonoBehaviour
 	{
         float h = Input.GetAxis("Horizontal");
         print("水平值:" + h);
+
         float v = Input.GetAxis("Vertical");
         print("垂直值:" + v);
 
         rig.velocity = new Vector2(h, v) * moveSpeed;
+
+        //h 不等於 0 或者 v 不等於 0 要走路
+        ani.SetBool(parRun, h != 0 || v!=0 );
 	}
 
 }
